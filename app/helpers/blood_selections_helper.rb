6e333ebd0660083f87d798c3b0e834977b0a144f
Joost@Mips <joost.van.averbeke@mips.be>
2015-10-13 12:44:49 +0200

    Added new jquery mobile theme with more colors. Added blood selection  status dependent theme colors

--- app/helpers/blood_selections_helper.rb
+++ app/helpers/blood_selections_helper.rb
@@ -1,2 +1,11 @@
 module BloodSelectionsHelper
+  def get_status_theme(status)
+    theme = case status
+              when 2 then 'e'
+              when 3 then 'd'
+              when 4 then 'c'
+              when 5 then 'f'
+              else 'a'
+    end
+  end
 end
